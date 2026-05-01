5cbc4672eb3b210d457841b5da76e9f0658b2568
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-05-01 20:16:41 +0200

    Added a img from every category & updated permalinks

--- template-parts/project-cards.php
+++ template-parts/project-cards.php
@@ -2,18 +2,57 @@
     <h3>Projects</h3>
     <div class="project-cards-container">
         <div class="project-card">
-            <a href="<?php echo get_permalink(21); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/skvaller.jpg"></a>
+            <a href="<?php echo get_permalink(25); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/alkov/alkov10.jpeg"></a>
         </div>
 
         <div class="project-card">
-            <a href="<?php echo get_permalink(21); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/dugges-ipa.jpg"></a>
+            <a href="<?php echo get_permalink(16); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/tapas/tapas.png"></a>
         </div>
 
         <div class="project-card">
-            <a href="<?php echo get_permalink(21); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/tapas.jpg"></a>
+            <a href="<?php echo get_permalink(29); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/winebar-designs/2vinino-natten.png"></a>
+        </div>
+
+        <div class="project-card">
+            <a href="<?php echo get_permalink(27); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/sjomat/Mockup katalog_sjomat4.png"></a>
+        </div>
+
+        <div class="project-card">
+            <a href="<?php echo get_permalink(18); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/1.POWerprint.png"></a>
         </div>
     </div>
-    <div class="all-projects-btn">
-        <a href="<?php echo get_permalink(21); ?>" class="projects-link">All projects</a>
+
+    <div class="concepts-container">
+        <h4>Concepts</h4>
+
+        <div class="project-cards-container">
+            <div class="project-card">
+                <a href="<?php echo get_permalink(31); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/pitchfork/pitchfork.png">
+                    <div class="img-info">
+                        <span class="title">Pitchfork</span>
+                        <div class="badges">
+                            <span class="badge">Magazine</span>
+                            <span class="badge">Concept</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="project-card">
+                <a href="<?php echo get_permalink(13); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/dugges/haze ipa. 1080x1920png.png"></a>
+            </div>
+
+            <div class="project-card">
+                <a href="<?php echo get_permalink(33); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/binas historia/front-binashistoria-cropped.png"></a>
+            </div>
+
+            <div class="project-card">
+                <a href="<?php echo get_permalink(35); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/potejto/Takeaway-bag-cropped.png"></a>
+            </div>
+
+            <div class="project-card">
+                <a href="<?php echo get_permalink(37); ?>" class="project-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/potejto/Takeaway-bag-cropped.png"></a>
+            </div>
+        </div>
     </div>
 </section>
