fe523b2656adb606c528c51f843ccbcab3a08345
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-05-01 20:15:07 +0200

    Updated header with link to redirect to start from project pages

--- header.php
+++ header.php
@@ -20,9 +20,6 @@
 
 <body>
     <header>
-        <!-- <nav class="nav-container">
-            <ul role="list" class="nav-grid">
-                <li id="nav-img"> -->
         <a href="/" class="signature-fixed">
             <span>Alma</span>
             <span>Hammar</span>
@@ -32,52 +29,31 @@
             <div class="hamb-line"></div>
             <div class="hamb-line"></div>
         </button>
-        <!-- </li> -->
 
-        <!-- <li id="links" class="nav-list"> -->
         <nav class="nav-top">
-            <a href="#projects" class="nav-link">Projects</a>
+            <?php if (is_front_page()): ?>
+                <a href="#projects" class="nav-link">Projects</a>
+            <?php else: ?>
+                <a href="/" class="nav-link">Start</a>
+            <?php endif; ?>
             <a href="#contact" class="nav-link">Contact</a>
         </nav>
-        <!--      </li>
-                <li id="w-node-_76044d79-fc02-f18d-681f-a792281eef66-281eef23">
-                    <div class="navigation__menu-btn">
-                        <div data-w-id="76044d79-fc02-f18d-681f-a792281eef68" class="open-wrap-2"></div>
-                        <div class="close-wrap">
-                            <div class="navigation-text-2">Close</div>
-                            <div class="close-btn"></div>
-                        </div>
-                </li>
-            </ul> -->
 
         <!-- Hamburger-overlay -->
         <div class="main-navigation" aria-hidden="true">
             <button class="close-btn"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/close-svgrepo-com.svg"></button>
             <div class="nav-items-container">
-                <a href="#projects" class="nav-link-overlay">Projects</a>
+                <?php if (is_front_page()): ?>
+                    <a href="#projects" class="nav-link-overlay">Projects</a>
+                <?php else: ?>
+                    <a href="/" class="nav-link-overlay">Start</a>
+                <?php endif; ?>
                 <a href="#contact" class="nav-link-overlay">Contact</a>
             </div>
-            <div class="navigation-footer">
+            <!--  <div class="navigation-footer">
                 <a href="https://instagram.com/hammarensgraf_" class="nav-contact-link">@hammarensgraf_</a>
-                <a href="mailto:almahammar@gmail.com" class="nav-contact-link">almahammar@gmail.com</a>
+                <a href="mailto:almahammar@hotmail.com" class="nav-contact-link">almahammar@hotmail.com</a>
                 <a href="https://linkedin.com/in/alma-hammar-0336011b4" class="nav-contact-link">LinkedIn</a>
-            </div>
+            </div> -->
         </div>
-
-        <!--     <div class="main-wrapper">
-                <div class="main-navigation" style="display: flex; filter: blur(0px); transform: translate3d(0px, 0vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
-                    <div class="nav-items-container">
-                        <a href="#projects" class="nav-link-overlay">Projects</a>
-                        <a href="#about" class="nav-link-overlay">About</a>
-                        <a href="#contact" class="nav-link-overlay">Contact</a>
-                    </div>
-
-                    <div class="navigation-footer">
-                        <a href="https://instagram.com/hammarensgraf_" class="nav-contact-link">@hammarensgraf_</a>
-                        <a href="mailto:bu@spkt.se" class="nav-contact-link">almahammar@gmail.com</a>
-                        <a href="linkedin.com/in/alma-hammar-0336011b4" class="nav-contact-link">LinkedIn</a>
-                    </div>
-                </div>
-            </div>
-        </nav> -->
     </header>
