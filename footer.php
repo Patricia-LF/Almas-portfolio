8225daa318939070373d2542159a24ba8ab60415
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-05-01 20:13:42 +0200

    Added contact links to footer

--- footer.php
+++ footer.php
@@ -4,7 +4,30 @@
 </div>
 
 <footer>
-    <a href="mailto:almahammar@gmail.com" class="footer-contact-link">almahammar@gmail.com</a>
+    <ul class="footer-links">
+        <li class="footer-link">
+            <a href="https://www.instagram.com/hammarensgraf_"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-2-1-logo-svgrepo-com.svg">
+            </a>
+        </li>
+        <li class="footer-link">
+            <a href="linkedin.com/in/alma-hammar-0336011b4"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/linkedin-icon-2-logo-svgrepo-com.svg">
+            </a>
+        </li>
+        <li class="footer-link">
+            <a href="mailto:almahammar@hotmail.com" class="footer-contact-link">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/email.svg">
+            </a>
+        </li>
+        <li class="footer-link">
+            <a href="tel:+46764143363" class="contact-link">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/phone.png">
+            </a>
+        </li>
+    </ul>
+
+    <div class="date">
+        <p>&copy; <?php echo date("Y"); ?> Alma Hammar</p>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
